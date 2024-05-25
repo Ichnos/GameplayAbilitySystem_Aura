e76822fab35e8a53d6037db027ee5d06957c719e
Ichnos <riccardo.decandia@gmail.com>
2024-05-25 16:23:27 +0200

    Capitolo 1

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -1,16 +1,23 @@
 // Copyright by Ichnos
 
-
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
 
-#include "Character/AuraEnemy.h"
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
-{
-
+{	
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }

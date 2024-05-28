7354f469b9459abeff52c5685f64def9435ec751
Ichnos <riccardo.decandia@gmail.com>
2024-05-28 20:05:38 +0200

    Widget and UI

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright by Ichnos
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright by Ichnos
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
